990a4b50f20e804c0351fabffe248242cc78b488
sassac <sassacmellon@gmail.com>
2026-05-12 17:00:02 +0900

    디스코드 메세지 전송 함수 생성

---
+++ new_news.py
@@ -0,0 +1,88 @@
+import feedparser
+import sqlite3
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url = "https://www.boannews.com/media/news_rss.xml"
+feed = feedparser.parse(url)
+news_db = sqlite3.connect("news.db")
+cur = news_db.cursor()
+cur.execute("""
+        CREATE TABLE IF NOT EXISTS news (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            link TEXT UNIQUE
+        )
+    """)
+Token = os.getenv("Token")
+news_channel_id = int(os.getenv("news_channel_id"))
+client = discord.Client(intents=discord.Intents.default())
+
+def parse_rss(): #RSS정규화
+    feed_list = []
+    feed_dict = {}
+    for entry in feed.entries:
+        title = entry.title
+        link = entry.link
+        feed_dict = {"title": title, "link": link}    
+        feed_list.append(feed_dict)
+    return feed_list
+
+def new_verification(): #신규 뉴스 검증
+    feeds = parse_rss()
+    cur = news_db.cursor()
+    for feed in feeds:
+        link = feed["link"]
+        
+        cur.execute("SELECT link FROM news WHERE link = ?", (link,))
+        result = cur.fetchone()
+
+        if result is None:
+            feed["new"] = "o"
+        else:
+            feed["new"] = "x"
+
+    return feeds
+
+async def send_message(): #디스코드 메세지 전송
+    feeds = new_verification()
+
+    for feed in feeds:
+        title = feed["title"]
+        link = feed["link"]
+
+        if feed["new"] == "o":
+            channel = client.get_channel(news_channel_id)
+
+            embed = discord.Embed(
+                title="새로운 보안 뉴스",
+                color=discord.Color.red()
+            )
+
+            embed.add_field(
+                name="제목",
+                value=title,
+                inline=False
+            )
+
+            embed.add_field(
+                name="링크",
+                value=link,
+                inline=False
+            )
+
+            embed.set_footer(text="보안 뉴스 봇")
+
+            await channel.send(embed=embed)
+
+        else:
+            continue
+            
+@client.event
+async def on_ready():
+    await send_message()
+
+client.run(Token)
